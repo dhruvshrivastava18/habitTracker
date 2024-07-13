976fd83754c659c84666e583b6af0e198ebd0dd5
dhruvswipe <156196543+dhruvswipe@users.noreply.github.com>
2024-07-13 19:23:15 +0530

    initial commit

---
+++ ToDoLIst/ToDoLIstApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoLIstApp.swift
+//  ToDoLIst
+//
+//  Created by Dhruv's Macbook Pro  on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoLIstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
